7b2697004accd27815a44a6d09572ff32e4692b6
Ben Sabler <ben.sabler@velocityprint.com>
2025-10-09 12:53:04 -0400

    setup player sprite and filesystem

---
+++ assets/assets.go
@@ -0,0 +1,31 @@
+package assets
+
+import (
+	"embed"
+	"image"
+	_ "image/png"
+
+	"github.com/hajimehoshi/ebiten/v2"
+)
+
+//go:embed *
+
+var assets embed.FS
+
+var PlayerSprite = mustLoadImage("images/player.png")
+
+func mustLoadImage(name string) *ebiten.Image {
+	file, err := assets.Open(name)
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	img, _, err := image.Decode(file)
+	if err != nil {
+		panic(err)
+	}
+
+	return ebiten.NewImageFromImage(img)
+
+}
