7a483633405cee9daf105c7c4541435c9d361e18
Ben Sabler <ben.sabler@velocityprint.com>
2025-10-09 13:27:12 -0400

    implemented accelerating forward

--- player.go
+++ player.go
@@ -7,11 +7,19 @@ import (
 	"github.com/hajimehoshi/ebiten/v2"
 )
 
-const rotationPerSecond = math.Pi
+const (
+	rotationPerSecond = math.Pi
+	maxAcceleration   = 8.0
+)
+
+var curAcceleration float64
 
 type Player struct {
-	sprite   *ebiten.Image
-	rotation float64
+	game           *Game
+	sprite         *ebiten.Image
+	rotation       float64
+	position       Vector
+	playerVelocity float64
 }
 
 func NewPLayer(game *Game) *Player {
@@ -19,6 +27,7 @@ func NewPLayer(game *Game) *Player {
 
 	p := &Player{
 		sprite: sprite,
+		game:   game,
 	}
 
 	return p
@@ -40,6 +49,8 @@ func (p *Player) Draw(screen *ebiten.Image) {
 	op.GeoM.Rotate(p.rotation)
 	op.GeoM.Translate(halfWidth, halfHeight)
 
+	op.GeoM.Translate(p.position.X, p.position.Y)
+
 	screen.DrawImage(p.sprite, op)
 }
 
@@ -47,11 +58,37 @@ func (p *Player) Update() {
 
 	speed := rotationPerSecond / float64(ebiten.TPS())
 
+	// rotate left
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) {
 		p.rotation -= speed
 	}
 
+	// rotate right
 	if ebiten.IsKeyPressed(ebiten.KeyRight) {
 		p.rotation += speed
+	}
+
+	p.accelerate()
+}
+
+func (p *Player) accelerate() {
+	if ebiten.IsKeyPressed(ebiten.KeyUp) {
+		if curAcceleration < maxAcceleration {
+			curAcceleration = p.playerVelocity + 4
+		}
+
+		if curAcceleration >= 8 {
+			curAcceleration = 8
+		}
+
+		p.playerVelocity = curAcceleration
+
+		// Move in the direction we are pointing.
+		dx := math.Sin(p.rotation) * curAcceleration
+		dy := math.Cos(p.rotation) * -curAcceleration
+
+		// Move the player on the screen.
+		p.position.X += dx
+		p.position.Y += dy
 	}
 }
